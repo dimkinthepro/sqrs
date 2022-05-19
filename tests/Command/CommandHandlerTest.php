58497afe3c7ad92d44260927ab772f40282dc244
DimkinThePro <DimkinThePro@gmail.com>
2022-05-19 20:04:28 +0500

    tests

--- tests/Command/CommandHandlerTest.php
+++ tests/Command/CommandHandlerTest.php
@@ -4,25 +4,43 @@ declare(strict_types=1);
 
 namespace Dimkinthepro\SqrsBundle\Tests\Command;
 
+use Dimkinthepro\SqrsBundle\Command\CommandBus;
+use Dimkinthepro\SqrsBundle\Command\CommandBusInterface;
 use Dimkinthepro\SqrsBundle\Tests\Command\Fixtures\Book;
 use Dimkinthepro\SqrsBundle\Tests\Command\Fixtures\CreateBookCommand;
 use Dimkinthepro\SqrsBundle\Tests\Command\Fixtures\CreateBookHandlerCommand;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Messenger\Handler\HandlersLocator;
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
 
 class CommandHandlerTest extends TestCase
 {
     public function testCase(): void
     {
-        $handler = new CreateBookHandlerCommand();
+        $commandBus = $this->createCommandBus();
 
         $author = 'John Doe';
         $date = new \DateTimeImmutable();
         $command = new CreateBookCommand($author, $date);
 
-        $newBook = $handler($command);
+        $newBook = $commandBus->execute($command);
 
         self::assertInstanceOf(Book::class, $newBook);
         self::assertEquals($newBook->date, $date);
         self::assertEquals($newBook->author, $author);
+    }
+
+    private function createCommandBus(): CommandBusInterface
+    {
+        $handler = new CreateBookHandlerCommand();
+
+        $messageBus = new MessageBus([
+            new HandleMessageMiddleware(new HandlersLocator([
+                CreateBookCommand::class => [$handler],
+            ])),
+        ]);
+
+        return new CommandBus($messageBus);
     }
 }
--- tests/Query/QueryHandlerTest.php
+++ tests/Query/QueryHandlerTest.php
@@ -4,20 +4,25 @@ declare(strict_types=1);
 
 namespace Dimkinthepro\SqrsBundle\Tests\Query;
 
+use Dimkinthepro\SqrsBundle\Query\QueryBus;
+use Dimkinthepro\SqrsBundle\Query\QueryBusInterface;
 use Dimkinthepro\SqrsBundle\Tests\Query\Fixtures\Book;
 use Dimkinthepro\SqrsBundle\Tests\Query\Fixtures\GetBookQuery;
 use Dimkinthepro\SqrsBundle\Tests\Query\Fixtures\GetBookQueryHandler;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Messenger\Handler\HandlersLocator;
+use Symfony\Component\Messenger\MessageBus;
+use Symfony\Component\Messenger\Middleware\HandleMessageMiddleware;
 
 class QueryHandlerTest extends TestCase
 {
     /** @dataProvider providerTestCase */
     public function testCate(string $author, bool $assertion): void
     {
-        $handler = new GetBookQueryHandler();
+        $queryBus = $this->createQueryBus();
         $command = new GetBookQuery($author);
 
-        $book = $handler($command);
+        $book = $queryBus->execute($command);
         if (false === $assertion) {
             self::assertNull($book);
         } else {
@@ -32,5 +37,18 @@ class QueryHandlerTest extends TestCase
             'success' => ['Doe John', true],
             'false' => ['John Doe', false],
         ];
+    }
+
+    private function createQueryBus(): QueryBusInterface
+    {
+        $handler = new GetBookQueryHandler();
+
+        $messageBus = new MessageBus([
+            new HandleMessageMiddleware(new HandlersLocator([
+                GetBookQuery::class => [$handler],
+            ])),
+        ]);
+
+        return new QueryBus($messageBus);
     }
 }
