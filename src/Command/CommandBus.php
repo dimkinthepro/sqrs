0fcfae20032f96c87c0c3ffc28cf0001b5c47ca7
DimkinThePro <DimkinThePro@gmail.com>
2022-05-19 16:28:52 +0500

    add bus realization

---
+++ src/Command/CommandBus.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dimkinthepro\SQRS\Command;
+
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class CommandBus implements CommandBusInterface
+{
+    use HandleTrait;
+
+    public function __construct(MessageBusInterface $commandBus)
+    {
+        $this->messageBus = $commandBus;
+    }
+
+    public function execute(CommandInterface $command): mixed
+    {
+        return $this->handle($command);
+    }
+}
---
+++ src/Query/QueryBus.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dimkinthepro\SQRS\Query;
+
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class QueryBus implements QueryBusInterface
+{
+    use HandleTrait;
+
+    public function __construct(MessageBusInterface $queryBus)
+    {
+        $this->messageBus = $queryBus;
+    }
+
+    public function execute(QueryInterface $query): mixed
+    {
+        return $this->handle($query);
+    }
+}
